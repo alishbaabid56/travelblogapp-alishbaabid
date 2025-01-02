b1a0d85307ec4d595ecb609c3b7fb9dc3a1db807
Alishba Abid <164169188+alishbaabid56@users.noreply.github.com>
2025-01-02 23:22:30 +0500

    Add files via upload

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,7 +16,7 @@ export default function AboutUs() {
         <div className="w-full md:w-1/2 mt-6 md:mt-0 space-y-6">
           <h1 className="text-3xl font-bold leading-tight text-gray-800">About David Leiter</h1>
           <p className="text-gray-600">
-            Thanks for looking! I'm David Leiter, the guy behind this website. I'm an American who's been traveling the world full-time for 9 years now.
+            Thanks for looking! I am David Leiter, the guy behind this website. I am an American who has been traveling the world full-time for 9 years now.
           </p>
           <p className="text-gray-600">
             I started this travel blog in 2019 to document my international trips, share my photos, and help others learn how to travel the world and find some really good spots off the beaten path.
@@ -25,10 +25,10 @@ export default function AboutUs() {
             All of the writing and pictures on this site come from my personal experience.
           </p>
           <p className="text-gray-600">
-            I'm currently based in Bali, Indonesia, where I met my wife Intan, who’s a Bali local. Together, we've done some bucket list hikes, climbed active volcanoes, seen exotic wildlife, and visited spectacular castles, temples, and monuments around the world.
+            I am currently based in Bali, Indonesia, where I met my wife Intan, who is a Bali local. Together, we have done some bucket list hikes, climbed active volcanoes, seen exotic wildlife, and visited spectacular castles, temples, and monuments around the world.
           </p>
           <p className="text-gray-600">
-            I've worked with and been featured by BBC Travel, NBC News, Time, and other companies. I haven’t been everywhere, but it’s on my list. I hope this world travel blog can help and inspire you in your own journeys as well!
+            I have worked with and been featured by BBC Travel, NBC News, Time, and other companies. I haven’t been everywhere, but it is on my list. I hope this world travel blog can help and inspire you in your own journeys as well!
           </p>
         </div>
       </div>
